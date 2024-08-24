be9d52a008d4deb7550d902abfb5032935b7dc8d
Rifat Hossain Khan <rifat.hk889@gmail.com>
2024-08-25 02:05:38 +0600

    yml file added

--- services/cache.js
+++ services/cache.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 const redis = require("redis");
 const util = require("util");
+const keys = require("../config/keys");
 
-const redisURL = "redis://127.00.1:6379";
-
-const client = redis.createClient(redisURL);
+const client = redis.createClient(keys.redisURL);
 
 client.hget = util.promisify(client.hget);
 
